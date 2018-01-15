a316219b1ce4d8262925940551da22ee54ad37f5
Ramon Ferreira Silva <ramon.silva@m4u.com.br>
2018-01-15 18:39:23 -0200

    Cnversor de imagens

--- scale_converter.py
+++ scale_converter.py
@@ -5,7 +5,16 @@ import cv2
 from PIL import Image
 
 
+DATA_DIR = "/home/rsilva/Projects/cefet/dataset"
+IMAGE_DIR = os.path.join(DATA_DIR, "imagenet")
+NEW_IMAGE_DIR = os.path.join(DATA_DIR, "convertido")
 
 
-img = Image.open(img_name).convert('RGB')
-# your own image operations 
+for img_file in os.listdir(IMAGE_DIR):        
+    im = Image.open( os.path.join(DATA_DIR,img_file))
+    if(len(im.shape)<3):
+        try:
+            print(img_file, "is greyscale")
+            im.convert('RGB').save(img_file)
+        except:
+            print("Deu merda")
