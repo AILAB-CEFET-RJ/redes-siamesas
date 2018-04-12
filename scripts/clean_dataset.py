de4c017474f59989371fee0201c952a7b35580fa
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2018-04-12 07:52:16 -0300

    Melhorias nos scripts

--- scripts/clean_dataset.py
+++ scripts/clean_dataset.py
@@ -1,16 +1,31 @@
 import os
 import sys
 from PIL import Image
+import hashlib
 
 
-DATA_DIR = "/Volumes/Externo/cefet/dataset/"
-IMAGE_DIR = os.path.join(DATA_DIR, "imagenet")
+DATA_DIR = os.environ["DATA_DIR"]
+IMAGENET_DIR = os.path.join(DATA_DIR, "imagenet")
+IMAGE_DIR = os.path.join(IMAGENET_DIR, "images")
 
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 for img_file in os.listdir(IMAGE_DIR):
     try:
         if(img_file != ".DS_Store"):
             im = Image.open(os.path.join(IMAGE_DIR, img_file))
+
+        if( md5(os.path.join(IMAGE_DIR, img_file)) == "880a7a58e05d3e83797f27573bb6d35c" ):
+            im.close()
+            os.remove(os.path.join(IMAGE_DIR, img_file))
+            print(img_file, "apagado")
     except:
         os.remove(os.path.join(IMAGE_DIR, img_file))
         print(img_file, "apagado")
+
+print("Finalizado")
--- scripts/gerar_triplas_vqa_imagenet.py
+++ scripts/gerar_triplas_vqa_imagenet.py
@@ -7,7 +7,7 @@ import logging
 DATA_DIR = os.environ["DATA_DIR"]
 VQA_DIR = os.path.join( os.path.join(os.path.join(DATA_DIR,"vqa"))  ,"mscoco")
 IMAGENET_DIR = os.path.join( os.path.join(os.path.join(DATA_DIR,"imagenet"))  ,"images")
-
+PAIRS_DIR =  os.path.join(os.path.join(DATA_DIR,"triplas"))
 
 #################################################################
 #               Configurando logs de execuçao                   #
@@ -23,19 +23,23 @@ logger.addHandler(logging.StreamHandler())
 logger.debug("DATA_DIR     : %s", DATA_DIR)
 logger.debug("VQA_DIR      : %s", VQA_DIR)
 logger.debug("IMAGENET_DIR : %s", IMAGENET_DIR)
+logger.debug("PAIRS_DIR    : %s", PAIRS_DIR)
+
 
 i = 0
 images_pairs = []
 for vqa_img in os.listdir(VQA_DIR):    
-    for imganet_img in os.listdir(IMAGENET_DIR):        
-        images_pairs.append( [ vqa_img, imganet_img ] )
-        if i % 100000 == 0:
-            logger.debug("{:d} pares de imagens gerados".format(i))
+    for imagenet_img in os.listdir(IMAGENET_DIR):     
+        images_pairs.append( [ vqa_img, imagenet_img ] )
+   
+    imagene_name = "{:s}_pairs.csv".format(vqa_img)
+    df = pd.DataFrame(images_pairs, columns=['mscoco','imagenet'])
+    df.to_csv(os.path.join(PAIRS_DIR, imagene_name), index=0, header=0)
+    
+    if i % 1000 == 0:
+        logger.debug("{:d} pares de imagens gerados".format(i))
+    images_pairs = []
     i = i + 1
-
-logger.info("salvando arquivo de pares em {:s}".format(os.path.join(DATA_DIR, "image_pairs.csv")))
-df = pd.DataFrame(images_pairs, columns=['mscoco','imagenet'])
-df.to_csv(os.path.join(DATA_DIR, "image_pairs.csv"), index=0, header=0)
 
 logger.info("Finalizado")
 
